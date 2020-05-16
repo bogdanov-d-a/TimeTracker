305c33d44518e31afd6c58868027e58fb68523cf
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2020-05-17 01:04:03 +0300

    Break code into 3 modules

---
+++ ed_time_tracker/basic.py
@@ -0,0 +1,26 @@
+from ed_time_tracker.common import *
+import edpu.query_window
+import edpu.file_utils
+
+def get_stats(actions):
+    result = ''
+
+    rb = ReportBuilder3()
+    for action in actions:
+        apply_action(rb, action)
+
+    warnings = rb.get_warnings()
+    if len(warnings) > 0:
+        for warning in warnings:
+            result += warning + '\n'
+        result += '\n'
+
+    result += rb.get_summary()
+
+    return result
+
+def stats_viewer(data_filename):
+    def data_provider():
+        data = edpu.file_utils.eval_file(data_filename)
+        return get_stats(data[0][data[1]])
+    edpu.query_window.run_with_exception_wrapper(data_provider, 'Time stats')
